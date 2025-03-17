a9f63db806b4071eac7f46539b0e68ff6fd6b3f8
ShivamShah1 <93140921+ShivamShah1@users.noreply.github.com>
2025-03-17 00:39:42 -0700

    intuition and ideology
    After reading the assignment, this was my intuition and ideology and will try to accomplish this

---
+++ Joby_C_assignment.cpp
@@ -0,0 +1,68 @@
+/* 
+Author: Shivam Shah
+Reason: Joby Aviation C assignment
+Date: 03/17/25
+Time: 12:02 AM
+
+To perform an eVTOL Simulation Problem
+
+There are five companies developing eVTOL aircraft with the some different characteristics.
+Trying to simulate using 20 random vehicles with 3 chargers from the given 5 companies for 3 hours,
+to calculate the following
+- average flight time per flight
+- average distance traveled per flight
+- average time charging per charge session
+- total number of faults
+- total number of passenger miles. 
+
+with these assumptions:
+- Each vehicle starts the simulation with a fully-charged battery
+- Each vehicle instantaneously reaches Cruise Speed
+- Each vehicle is airborne for the full use of the battery, and is immediately in line for the charger after running out of battery power.
+
+Intuition and ideology:
+All the selected vehicles will have full battery power so all the 20 vehicles will be flying in the air,
+so calculating the flight time for all vehicles with full charged battery and the vehicles with the respective 
+flight range and time will land respectively and will try to accuire the charging station if free or will wait for its avaibility and 
+then set its flight again. We will try to run this scenario for 3 hours.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/* Vehicle class encapsulating eVTOL properties and statistics */
+class Vehicle {
+    
+};
+
+/* Simulate the flight */
+void simulate_flight() {
+    
+}
+
+/* Charger class to manage charging stations */
+class Charger {
+
+};
+
+int main() {
+    /* creating 5 objects of vehicles */
+    
+    /* picking random 20 vehicles form the objects created above with equal picking probability */
+    
+    /* creating a vector to store the information of the 20 vehicles */
+    
+    /* try to simulate the flight till it reaches its capacity */
+    
+    /* creating a vehicle chaging queue */
+    
+    /* allocating the charging stations */
+    
+    /* updating the global vehicle values */
+    
+    /* printing the result */
+    
+    return 0;
+}
